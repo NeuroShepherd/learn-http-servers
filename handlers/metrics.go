4cdb85d7429f859b0387c198604ad441c70c9739
Patrick Callahan <patricktcallahan18@gmail.com>
2026-03-25 09:38:25 +0100

    Abstract metrics and reset handlers to handlers module

---
+++ handlers/metrics.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type APIConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *APIConfig) MiddlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *APIConfig) GetFileserverHits() int32 {
+	return cfg.fileserverHits.Load()
+}
+
+func (cfg *APIConfig) HandlerMetrics() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hits: " + fmt.Sprintf("%d", cfg.GetFileserverHits())))
+	})
+}
+
+func (cfg *APIConfig) HandlerMetricsReset() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+}
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"sync/atomic"
 
 	"github.com/neuroshepherd/learn-http-servers/handlers"
 )
@@ -15,36 +13,11 @@ func main() {
 		Addr:    ":8080",
 	}
 
-	cfg := &apiConfig{}
-	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	mux.Handle("/metrics", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hits: " + fmt.Sprintf("%d", cfg.getFileserverHits())))
-	}))
-	mux.Handle("/reset", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Store(0)
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	}))
-
+	cfg := &handlers.APIConfig{}
+	mux.Handle("/app/", cfg.MiddlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.Handle("/metrics", cfg.HandlerMetrics())
+	mux.Handle("/reset", cfg.HandlerMetricsReset())
 	mux.HandleFunc("/healthz", handlers.HandlerHealth())
 
 	server.ListenAndServe()
-}
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) getFileserverHits() int32 {
-	return cfg.fileserverHits.Load()
 }
