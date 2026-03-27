1d2183c68bc7efd8b4147b3eb8414e66422b3959
Patrick Callahan <patricktcallahan18@gmail.com>
2026-03-27 16:06:21 +0100

    Implement funcs for making and validating jwts

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -3,9 +3,47 @@ package auth
 import (
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
 
 func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
 
+	tokenString, err := jwtToken.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrTokenUnverifiable
+		}
+		return []byte(tokenSecret), nil
+	}
+	jwtTokenResp, err := jwt.ParseWithClaims(tokenString, &claims, keyFunc)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if !jwtTokenResp.Valid {
+		return uuid.Nil, jwt.ErrTokenInvalidClaims
+	}
+
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userID, nil
 }
