917bea08055912131bccb2818747d80a13753530
Patrick Callahan <patricktcallahan18@gmail.com>
2026-03-28 13:04:27 +0100

    Create query for refreshing tokens

--- sql/queries/tokens.sql
+++ sql/queries/tokens.sql
@@ -2,3 +2,10 @@
 INSERT INTO refresh_tokens (token, created_at, updated_at, user_id, expires_at)
 VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
+
+-- name: GetUserFromRefreshToken :one
+SELECT users.* FROM users
+JOIN refresh_tokens ON users.id = refresh_tokens.user_id
+WHERE refresh_tokens.token = $1
+AND revoked_at IS NULL
+AND expires_at > NOW();
